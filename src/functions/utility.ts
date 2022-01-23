31a7d97a925d01edf2ee77869313ab73741ddd68
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-01-23 20:42:23 +0800

    increased accurary and simplicity with dayjs

--- src/functions/utility.ts
+++ src/functions/utility.ts
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+dayjs.extend(duration)
+
 export const changeBool = (x: boolean) => {
   return x ? false : true
 }
@@ -19,40 +23,15 @@ export const convertDeg2Arc = (deg: number) => {
 }
 
 export const generateDataLabels = async (generateData: Function) => {
-  const date = new Date()
-  let hourNow = date.getHours()
-  let minuteNow = date.getMinutes()
-
   const labels: any = []
   const data: any = []
 
-  let hours, minutes
-  for (let i = 0; i < 24; i++) {
-    if (minuteNow < 30) {
-      if (i % 2 === 0) {
-        hours = hourNow
-      } else {
-        hours = hourNow
-        hourNow += 1
-      }
-      minutes = i % 2 === 0 ? '00' : '30'
-    } else {
-      if (i % 2 === 0) {
-        hours = hourNow
-      } else {
-        hours = hourNow + 1
-        hourNow += 1
-      }
-      minutes = i % 2 === 0 ? '30' : '00'
-    }
-    // Check if hours is larger than 24
-    hours = hours >= 24 ? hours - 24 : hours
-    labels.push({
-      hour: hours,
-      min: minutes
-    })
-    data.push(await generateData(new Date(new Date().setHours(Number(hours), Number(minutes)))))
-  }
+  let time = dayjs()
+  for (let i = 0; i < 22; i++) {
+    time.minute() > 30 ? labels.push(`${time.hour()}:30`) : labels.push(`${time.hour()}:00`)
+    data.push(await generateData(time.toDate()))
+    time = time.add(dayjs.duration({ 'minutes': 30 }))
+  } 
   return {
     labels,
     data
@@ -62,7 +41,7 @@ export const generateDataLabels = async (generateData: Function) => {
 export const generateGraph = async (title: string, gridColor: string, backgroundColor: string, labels: any, data: any) => {
   const graphConfig = {
     data: {
-      labels: [...labels.map((label: { hour: string; min: string }) => `${label.hour}:${label.min}`)],
+      labels: labels,
       datasets: [
         {
           type: 'bar',
