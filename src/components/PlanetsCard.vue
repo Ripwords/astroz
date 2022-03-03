caf632245fce63a1b1830047fb3f43df8f844012
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-03-03 21:26:38 +0800

    update card

--- src/components/PlanetsCard.vue
+++ src/components/PlanetsCard.vue
@@ -8,21 +8,51 @@ const { chartKey, graphConfig } = await planetsCardsInit()
 </script>
 
 <template>
-  <ion-card v-if="!isDesktop">
+  <ion-card v-if="isDesktop">
     <ion-card-header>
       <ion-card-header>
         <ion-card-title>Planets</ion-card-title>
       </ion-card-header>
       <ion-card-content>
-        <div class="flex justify-start max-w-[175px]" v-for="(planet, index) in planetNames" :key="planet">
-          <div class="flex justify-between w-[75px]">
-            <div>
-              {{ planet }}
+        <div class="flex">
+          <div>
+            <div class="flex justify-start max-w-[175px]" v-for="(planet, index) in planetNames.slice(0, 3)" :key="planet">
+              <div class="flex justify-between w-[70px]">
+                <div>
+                  {{ planet }}
+                </div>
+                <div>:</div>
+              </div>
+              <span class="w-[10px]"></span>
+              <div class="w-[30px] h-auto opacity-50" :style="{ background: store.planetColors[index] }"></div>
             </div>
-            <div>:</div>
           </div>
-          <span class="w-[10px]"></span>
-          <div class="w-[30px] h-auto opacity-50" :style="{ background: store.planetColors[index] }"></div>
+          <span class="w-[20px]"></span>
+          <div>
+            <div class="flex justify-start max-w-[175px]" v-for="(planet, index) in planetNames.slice(3, 6)" :key="planet">
+              <div class="flex justify-between w-[70px]">
+                <div>
+                  {{ planet }}
+                </div>
+                <div>:</div>
+              </div>
+              <span class="w-[10px]"></span>
+              <div class="w-[30px] h-auto opacity-50" :style="{ background: store.planetColors[index + 3] }"></div>
+            </div>
+          </div>
+          <span class="w-[20px]"></span>
+          <div>
+            <div class="flex justify-start max-w-[175px]" v-for="(planet, index) in planetNames.slice(6)" :key="planet">
+              <div class="flex justify-between w-[70px]">
+                <div>
+                  {{ planet }}
+                </div>
+                <div>:</div>
+              </div>
+              <span class="w-[10px]"></span>
+              <div class="w-[30px] h-auto opacity-50" :style="{ background: store.planetColors[index + 6] }"></div>
+            </div>
+          </div>
         </div>
       </ion-card-content>
     </ion-card-header>
