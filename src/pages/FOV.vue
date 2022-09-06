50cd112ba835bdee26ee47541f36b64eea9d6939
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-09-06 17:40:56 +0800

    fas

--- src/pages/FOV.vue
+++ src/pages/FOV.vue
@@ -13,12 +13,9 @@ const {
 const accToggle = ref("first")
 const target = ref("")
 const doneLoad = ref()
-const rangeVal = ref(180)
+const rangeVal = ref(90)
 const frame = ref()
 const wrapper = ref()
-const pinFormatter = (val: number) => {
-  return `${val}`
-}
 const isErr = ref(false)
 
 const fov_x = ref(computed(() => {
@@ -47,7 +44,7 @@ const fetchFromAPI = () => {
   isErr.value = false
   frame.value.src = ""
   frame.value.style.border = "0px"
-  const data = retrieveFromAPI({ width: fov_x.value, height: fov_y.value }, scaling, getWidthOfParent() / fov_x.value, target.value)
+  const data = retrieveFromAPI({ width: fov_y.value, height: fov_x.value }, scaling, getWidthOfParent() / fov_x.value, target.value)
   frame.value.src = data
 }
 
@@ -88,7 +85,14 @@ const drawSensorFrame = () => {
 }
 
 const imgLoaded = () => {
-  frame.value.style.border = "5px solid white"
+  // detect light/dark mode
+  const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+  frame.value.style.border = isDark ? "5px solid white" : "5px solid black"
+  frame.value.transformOrigin = "center"
+  frame.value.style.transform = "rotate(-90deg)"
+  // offset the frame to the top to compensate for the rotation
+  frame.value.style.marginTop = `-${(frame.value.height - frame.value.width) / 2}px`
+  wrapper.value.style.marginTop = `-${(frame.value.height - frame.value.width) / 2}px`
   drawSensorFrame()
   doneLoad.value = true
 }
@@ -136,8 +140,8 @@ onUnmounted(() => {
               </div>
             </ion-accordion>
           </ion-accordion-group>
-          <CalcInput @keypress.enter="fetchFromAPI" v-model:val="target" label="Target Name" type="text" suffix="" />
-          <div class="flex justify-center">
+          <CalcInput v-model:val="target" label="Target Name" type="text" suffix="" />
+          <div class="flex justify-center mt-4">
             <ion-button @click="fetchFromAPI" :disabled="target == '' || !genFOV">Search
             </ion-button>
           </div>
@@ -151,14 +155,14 @@ onUnmounted(() => {
           <div class="flex justify-center" v-show="doneLoad && !isErr">
             <div class="w-[70vw] max-w-[400px]">
               <div class="flex justify-center">
-                <ion-range :pin="true" :pin-formatter="pinFormatter" :min="0" :max="360" v-model="rangeVal">
+                <ion-range :min="0" :max="180" v-model="rangeVal">
                   <i-ion:refresh slot="start"></i-ion:refresh>
                 </ion-range>
               </div>
             </div>
           </div>
         </Calc>
-        <div class="flex justify-center relative">
+        <div class="flex justify-center relative mx-2">
           <img v-show="!isErr" ref="frame" @load="imgLoaded()" @error="err" />
           <div ref="wrapper"></div>
           <ion-item v-show="isErr">
