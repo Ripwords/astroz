a862896c8d8f7466890065de15c20a939b5aedcb
JJ Teoh <teohjjteoh@gmail.com>
2022-09-09 21:08:23 +0800

    fix ccd table

--- src/components/SeeingTable.vue
+++ src/components/SeeingTable.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { isDesktop } from '../functions/utility'
-const table_width = isDesktop ? "400px" : "330px"  
+const table_width = isDesktop ? "400px" : "330px"
 const table_color = "#1e1e1e"
 </script>
 
@@ -22,7 +22,7 @@ const table_color = "#1e1e1e"
         </tr>
         <tr>
           <td>Good</td>
-          <td>1 - 2"</td> 
+          <td>1 - 2"</td>
           <td>0.33 - 1"</td>
         </tr>
         <tr>
@@ -46,31 +46,45 @@ const table_color = "#1e1e1e"
 </template>
 
 <style scoped>
+@media (prefers-color-scheme: light) {
+  .styled-table {
+    background-color: #ffffff;
+    color: black;
+  }
+}
+
+@media (prefers-color-scheme: dark) {
+  .styled-table {
+    background-color: #1b1b1b;
+    color: white;
+  }
+}
+
 .styled-table {
-    border-collapse: collapse;
-    font-size: 0.9em;
-    font-family: sans-serif;
-    min-width: v-bind(table_width);
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  border-collapse: collapse;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: v-bind(table_width);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
 }
 
 .styled-table thead tr {
-    background-color: v-bind(table_color);
-    color: #ffffff;
-    text-align: left;
+  background-color: v-bind(table_color);
+  color: #ffffff;
+  text-align: left;
 }
 
 .styled-table th,
 .styled-table td {
-    padding: 12px 15px;
+  padding: 12px 15px;
 }
 
 .styled-table tbody tr {
-    border-bottom: 1px solid #dddddd;
+  border-bottom: 1px solid #dddddd;
 }
 
 
 .styled-table tbody tr:last-of-type {
-    border-bottom: 2px solid v-bind(table_color);
+  border-bottom: 2px solid v-bind(table_color);
 }
 </style>
